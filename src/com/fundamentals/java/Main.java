fc476c14d2fe0670531deba4b45093cb12224001
Roger O'Dell <odell.instructor@gmail.com>
2021-06-10 16:04:09 -0500

    Created House Class

--- src/com/fundamentals/java/Main.java
+++ src/com/fundamentals/java/Main.java
@@ -5,5 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to Java");
+        System.out.println("This is my first sentence");
+        System.out.println("I am new to Java");
+        System.out.println("This Java course is cool");
+        System.out.println("I am learning stuff every day");
+
+        House myHouse = new House();
+        myHouse.doorOpenClose();
+
+        // Single line comment
+        /*
+         Multi-line comment
+         so I can use more words
+         */
+
     }
 }
